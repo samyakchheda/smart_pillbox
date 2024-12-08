c9e50a8c205395bb3cbae9f0d8c7d8dd15e580c1
Parth <parthforudemy@gmail.com>
2024-12-08 20:55:16 +0530

    New folder structure and files

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:io';
-import 'package:gallery_picker/gallery_picker.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,73 +25,10 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          List<MediaFile>? media = await GalleryPicker.pickMedia(
-              context: context, singleMedia: true);
-          if (media != null && media.isNotEmpty) {
-            var data = await media.first.getFile();
-            setState(() {
-              selectMedia = data;
-            });
-          }
-        },
-        child: const Icon(Icons.image),
-      ),
-      body: _buildUI(),
-    );
-  }
 
-  Widget _buildUI() {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [_imageView(), _extractedTextView()],
+      body: Center(
+        child: Text("Welcome To Home Screen"),
       ),
     );
-  }
-
-  Widget _imageView() {
-    if (selectMedia == null) {
-      return const Center(
-        child: Text("Please select the image for tet recognition"),
-      );
-    }
-    return Center(
-      child: Image.file(
-        selectMedia!,
-        width: 200,
-      ),
-    );
-  }
-
-  Widget _extractedTextView() {
-    if (selectMedia == null) {
-      return const Center(
-        child: Text("No result"),
-      );
-    }
-    return FutureBuilder(
-        future: _extractText(selectMedia!),
-        builder: (context, snapshot) {
-          return Text(
-            snapshot.data ?? "",
-            style: const TextStyle(fontSize: 15),
-          );
-        });
-  }
-
-  Future<String?> _extractText(File file) async {
-    final textRecognizer = TextRecognizer(
-      script: TextRecognitionScript.latin,
-    );
-    final InputImage inputImage = InputImage.fromFile(file);
-    final RecognizedText recognizedText =
-        await textRecognizer.processImage(inputImage);
-    String text = recognizedText.text;
-    textRecognizer.close();
-    return text;
   }
 }
